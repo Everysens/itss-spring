78118b0c94181cc8dc5d7ea2e06859093fd25681
idkw <valentin.baert@everysens.com>
2018-01-05 10:55:30 +0100

    feat(controller): add error log on itss ex

--- impl/src/main/java/com/everysens/itss/controller/error/ITSSExceptionHandler.java
+++ impl/src/main/java/com/everysens/itss/controller/error/ITSSExceptionHandler.java
@@ -2,18 +2,21 @@ package com.everysens.itss.controller.error;
 
 import com.everysens.itss.dto.error.ErrorDto;
 import com.everysens.itss.exception.ItssException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@Order(value = 1000)
+@Order(value = 1)
 @ControllerAdvice
+@Slf4j
 public class ITSSExceptionHandler {
 
     @ExceptionHandler(value = ItssException.class)
     public ResponseEntity<ErrorDto> handle(ItssException ex){
+        log.error("Caught ITSS error", ex);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getError());
     }
 
